e3d9fda080a992bef8a2d3f6916056759b5202f5
alumno Secundaria <al3201@cedi.local>
2018-10-16 08:08:13 -0500

    subir todo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinasPage } from '../bocinas/bocinas';
+import { TvPage } from '../tv/tv';
+import { SmartphonePage } from '../smartphone/smartphone';
+import { LamparaPage } from '../lampara/lampara';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  bocinas= BocinasPage;
+  tv= TvPage;
+  smart= SmartphonePage;
+  lamp= LamparaPage;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  clickBocinas(){
+    this.navCtrl.push(this.bocinas);
+  }
+
+  clickTV(){
+    this.navCtrl.push(this.tv);
+  }
+
+  clickSmartphone(){
+    this.navCtrl.push(this.smart);
+  }
+
+  clickLampara(){
+    this.navCtrl.push(this.lamp);
   }
 
 }
